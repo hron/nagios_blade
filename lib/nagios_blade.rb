1fb60aed70843bd86a72eb109d09a013b42fc973
Aleksei Gusev <aleksei.gusev@gmail.com>
2013-12-07 14:13:30 +0300

    Implement nagios blade checker.

--- lib/nagios_blade.rb
+++ lib/nagios_blade.rb
@@ -1,5 +1,41 @@
 require "nagios_blade/version"
 
+require 'nokogiri'
+require 'open-uri'
+require 'nagios-plugin'
+
 module NagiosBlade
-  # Your code goes here...
+  class Plugin < Nagios::Plugin
+    def run
+      @opts.banner = "Usage: nagios_blade [options] BLADE_URL"
+      @opts.parse!
+
+      @blade_url = ARGV[0]
+      unless @blade_url
+        puts @opts.help
+        exit(255)
+      end
+      @critical &&= @critical.to_f
+      @warning &&= @warning.to_f
+
+      status_html = Nokogiri::HTML(open(blade_full_url))
+      average_hashrate = status_html.css('center table tr:first-child td:last-child')[0].text.to_i
+      message = "Speed: %.2d Gh/s" % (average_hashrate / 1000)
+
+      if average_hashrate < @critical
+        critical(message)
+      elsif average_hashrate < @warning
+        warning(message)
+      else
+        ok(message)
+      end
+    end
+
+
+    private
+
+    def blade_full_url
+      "http://#{@blade_url}:8000"
+    end
+  end
 end
--- nagios_blade.gemspec
+++ nagios_blade.gemspec
@@ -23,4 +23,5 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "pry"
 
   spec.add_dependency "nagios-plugin"
+  spec.add_dependency "nokogiri"
 end
